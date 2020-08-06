b4f993c7b9512c27559c19774ba8fd7a954af6ec
KP <prateeqkaustuv@gmail.com>
2020-08-05 20:42:16 -0700

    04 Merged README and SourceTree, Added Screen Enum

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Hacker : MonoBehaviour
 {
     int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
 
     void Start()
     {
-        startSequence();
-        startGameLevel(level);
+        mainMenu();
     }
 
     void Update()
@@ -18,23 +19,31 @@ public class Hacker : MonoBehaviour
 
     }
 
-    void startSequence()
+    void mainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack into?\nPress 1 for the local library\nPress 2 for the police station\nPress 3 for the NASA\nEnter your selection:");
     }
 
     void OnUserInput(string input)
     {
-        if (input == "menu") startSequence();
-        else if (input == "1" || input == "2" || input == "3")
+        if (input == "menu") mainMenu();
+        if (currentScreen == Screen.MainMenu)
         {
-            level = int.Parse(input);
-            startGameLevel(level);
+            if (input == "1" || input == "2" || input == "3")
+            {
+                currentScreen = Screen.Password;
+                level = int.Parse(input);
+                startGameLevel(level);
+            }
+            else if (input.Length == 4) Terminal.WriteLine("Not your credit card password fool");
+            else Terminal.WriteLine("Please choose a valid input");
         }
-        else if (input.Length == 4) Terminal.WriteLine("Not your credit card password fool");
-        else Terminal.WriteLine("Please choose a valid input");
-
+        else if (currentScreen == Screen.Password)
+        {
+            ;
+        }
     }
 
 
