57606f56b6b308329595baeca4de0c47f6a73d13
KP <prateeqkaustuv@gmail.com>
2020-08-05 21:02:43 -0700

    05 Finished Game Loop

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
     int level;
+    string password;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
 
     void Start()
     {
-        mainMenu();
+        showMainMenu();
     }
 
     void Update()
@@ -19,46 +21,61 @@ public class Hacker : MonoBehaviour
 
     }
 
-    void mainMenu()
+    void showMainMenu()
     {
         currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack into?\nPress 1 for the local library\nPress 2 for the police station\nPress 3 for the NASA\nEnter your selection:");
     }
 
+    void runMainMenu(string input)
+    {
+        if (input == "1" || input == "2" || input == "3")
+        {
+            currentScreen = Screen.Password;
+            level = int.Parse(input);
+            Terminal.WriteLine("You have chosen level " + level + "\nPlease enter the password: ");
+        }
+        else if (input.Length == 4) Terminal.WriteLine("Not your credit card password fool");
+        else Terminal.WriteLine("Please choose a valid input");
+    }
+
     void OnUserInput(string input)
     {
-        if (input == "menu") mainMenu();
+        if (input == "menu")
+        {
+            showMainMenu();
+        }
         if (currentScreen == Screen.MainMenu)
         {
-            if (input == "1" || input == "2" || input == "3")
-            {
-                currentScreen = Screen.Password;
-                level = int.Parse(input);
-                startGameLevel(level);
-            }
-            else if (input.Length == 4) Terminal.WriteLine("Not your credit card password fool");
-            else Terminal.WriteLine("Please choose a valid input");
+            runMainMenu(input);
         }
         else if (currentScreen == Screen.Password)
         {
-            ;
+            startGame(input);
         }
     }
 
-
-    void startGameLevel(int level)
+    void winScreen()
     {
-        Terminal.WriteLine("You have chosen level " + level);
-        if (level == 1)
-        {; }
-        else if (level == 2)
+        Terminal.WriteLine("WELL DONE! YOU'RE MONKAS");
+    }
+
+    void startGame(string input)
+    {
+        checkPassword(input);
+    }
+
+    void checkPassword(string input)
+    {
+        if (input == password)
         {
-            ;
+            winScreen();
         }
-        else if (level == 3)
+        else
         {
-            ;
+            Terminal.WriteLine("Wrong password, try again!!");
+
         }
     }
 }
